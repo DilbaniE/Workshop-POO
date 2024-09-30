721708b12e3bb15d9746365a744762425093bcf4
DilbaniE <claudiaenriquez241@gmail.com>
2024-09-30 14:45:55 -0500

    Ejercicio numero 04

--- Workshop-POO/Persona.ts
+++ Workshop-POO/Persona.ts
@@ -1,14 +1,19 @@
 export class Persona {
     nombre: string
-    edad: number
+    private edad: number
 
     constructor(nombre: string, edad: number) {
         this.nombre = nombre
         this.edad = edad
     }
 
+    public getEdad(): number{
+        return this.edad;
+    }
     saludar(){
         console.log(`Tu nombre es: ${this.nombre} y tienes ${this.edad}`)
     }
+
+    
 }
 
