152c91573f8aa6fc1479d35f8dd035ed3bdd663e
DilbaniE <claudiaenriquez241@gmail.com>
2024-09-30 16:04:06 -0500

    Ejercicio numero 07

--- Workshop-POO/index.ts
+++ Workshop-POO/index.ts
@@ -1,6 +1,7 @@
 import { Persona } from "./Persona";
 import { Empleado } from "./Empleado";
 
+// persona
 const persona1 = new Persona("Dilbani", 25)
 persona1.saludar();
 
@@ -31,7 +32,30 @@ persona9.saludar();
 const persona10 = new Persona("Juana", 15)
 persona10.saludar();
 
-const empleado = new Empleado("Dilbani Enriquez", 25,150000)
+// Empleado 
+const empleado = new Empleado("Dilbani Enriquez", 25, 1590000)
 empleado.saludar();
 empleado.trabajar();
 
+const empleado1 = new Empleado("Luis", 45, 6350000)
+empleado1.saludar();
+const empleado2 = new Empleado("Fanny", 65, 3500000)
+empleado2.saludar();
+const empleado3 = new Empleado("Jaime", 75, 5500000)
+empleado3.saludar();
+const empleado4 = new Empleado("Luis", 45, 2500000)
+empleado4.saludar();
+const empleado5 = new Empleado("Fran", 85, 2700000)
+empleado5.saludar();
+const empleado6 = new Empleado("Jose", 75, 4500000)
+empleado6.saludar();
+const empleado7 = new Empleado("Olga", 45, 1500000)
+empleado7.saludar();
+const empleado8 = new Empleado("Camila", 29, 1800000)
+empleado8.saludar();
+const empleado9 = new Empleado("Ramro", 35, 1340000)
+empleado9.saludar();
+const empleado10 = new Empleado("Jass", 34, 4500000)
+empleado10.saludar();
+
+ 
